83fdcd213e40ec505bc1b93f9d02412e35b74516
Darius Weber <weber@xplace.de>
2018-09-21 09:34:36 +0200

    add example

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ProtectedComponent } from './components/protected/protected.component';
+import { NavigationComponent } from './components/navigation/navigation.component';
+import { ListComponent } from './components/list/list.component';
+import {MatSidenavModule, MatToolbarModule} from '@angular/material';
+import { TableComponent } from './components/table/table/table.component';
+import { TableBodyComponent } from './components/table/table-body/table-body.component';
+import { TableCellComponent } from './components/table/table-cell/table-cell.component';
+import { TableHeaderComponent } from './components/table/table-header/table-header.component';
+import { TableRowComponent } from './components/table/table-row/table-row.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProtectedComponent,
+    NavigationComponent,
+    ListComponent,
+    TableComponent,
+    TableBodyComponent,
+    TableCellComponent,
+    TableHeaderComponent,
+    TableRowComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatSidenavModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/list/list.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-list',
+  templateUrl: './list.component.html',
+  styleUrls: ['./list.component.css']
+})
+export class ListComponent implements OnInit {
+  possibleChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  content: string[][] = [];
+
+  ngOnInit() {
+    this.content = this.createContent(255);
+  }
+
+  private createContent(count: number): string[][] {
+    const content: string [][] = [];
+
+    for (let i = 0; i < count; i++) {
+      content.push([i + '' , this.randomString()]);
+    }
+
+    return content;
+  }
+
+  private randomString(): string {
+    let text = '';
+    const length = this.randomNumber(8, 13);
+
+    for (let i = 0; i < length; i++) {
+      text += this.possibleChars.charAt(this.randomNumber(0, this.possibleChars.length));
+    }
+
+    return text;
+  }
+
+  private randomNumber(min: number, max: number): number {
+    return Math.floor(Math.random() * max) + min;
+  }
+
+}
