10fec521cf3da6b9b4f8b127943e0815e07970c0
Darius <weber@xplace.de>
2018-09-24 08:49:45 +0200

    use sass

--- src/app/components/protected/protected.component.ts
+++ src/app/components/protected/protected.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-protected',
   templateUrl: './protected.component.html',
-  styleUrls: ['./protected.component.css']
+  styleUrls: ['./protected.component.sass']
 })
 export class ProtectedComponent implements OnInit {
 
